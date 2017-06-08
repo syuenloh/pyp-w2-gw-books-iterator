b2d398370e935858a3ddc9ea66bb9252841037e5
Martin Zugnoni <martin.zugnoni@gmail.com>
2017-06-08 19:10:59 -0300

    Add extra test case

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -6,6 +6,14 @@ from books_iterator.main import Book, Price, BookIterator
 
 class BookIteratorTestCase(unittest.TestCase):
 
+    def test_book_iterator_next_method(self):
+        books_iterator = BookIterator('books.csv')
+        it = iter(books_iterator)
+        book = next(it)
+        self.assertTrue(isinstance(book, Book))
+        self.assertTrue(isinstance(book.price, Price))
+        self.assertEqual(book.price.currency, 'USD')
+
     def test_book_list_comprehension_iteration(self):
         books_iterator = BookIterator('books.csv')
         self.assertEqual(len([b for b in books_iterator]), 7)
